0bb4ea9e47c43b67f14fe1b2e011c8b742650897
UnDuhDuhSea <tafimba@gmail.com>
2021-04-03 19:13:15 -0700

    Adding prj links to website

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var prj1 = document.getElementById('prj-1');
+var prj1Git = document.getElementById('prj-1-git');
+var prj2 = document.getElementById('prj-2');
+var prj2Git = document.getElementById('prj-2-git');
+var prj3 = document.getElementById('prj-3');
+var prj3Git = document.getElementById('prj-3-git');
+// var prj4 = document.getElementById('prj-4')
+// var prj4Git = document.getElementById('prj-1-git')
+// var prj5 = document.getElementById('prj-5')
+// var prj5Git = document.getElementById('prj-1-git')
+
+
+prj1.addEventListener('click', function () {
+    location.href = 'https://d606n6k.github.io/recipe-shuffle/';
+})
+// prj1Git.addEventListener('click', function () {
+//     location.href = 'https://github.com/UnDuhDuhSea/My-Portfolio';
+// })
+
+prj2.addEventListener('click', function () {
+    location.href = 'https://unduhduhsea.github.io/Weather-Dashboard/';
+})
+// prj2Git.addEventListener('click', function () {
+//     location.href = 'https://github.com/UnDuhDuhSea/Weather-Dashboard';
+// })
+
+prj3.addEventListener('click', function () {
+    location.href = 'https://unduhduhsea.github.io/Work-Day-Scheduler/';
+})
+// prj3Git.addEventListener('click', function () {
+//     location.href = 'https://github.com/UnDuhDuhSea/Work-Day-Scheduler';
+// })
