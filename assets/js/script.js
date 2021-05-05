75f6ea09967982aa2d71eae9e62fda1c70560dfd
UnDuhDuhSea <tafimba@gmail.com>
2021-05-04 17:47:15 -0700

    Adding links to other projects cards and updating resume

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,45 @@
-var prj1 = document.getElementById('prj-1');
-var prj1Git = document.getElementById('prj-1-git');
-var prj2 = document.getElementById('prj-2');
-var prj2Git = document.getElementById('prj-2-git');
-var prj3 = document.getElementById('prj-3');
-var prj3Git = document.getElementById('prj-3-git');
-// var prj4 = document.getElementById('prj-4')
-// var prj4Git = document.getElementById('prj-1-git')
-// var prj5 = document.getElementById('prj-5')
-// var prj5Git = document.getElementById('prj-1-git')
+var prj1 = document.getElementById("prj-1");
+var prj1Git = document.getElementById("prj-1-git");
+var prj2 = document.getElementById("prj-2");
+var prj2Git = document.getElementById("prj-2-git");
+var prj3 = document.getElementById("prj-3");
+var prj3Git = document.getElementById("prj-3-git");
+var prj4 = document.getElementById("prj-4");
+var prj4Git = document.getElementById("prj-1-git");
+var prj5 = document.getElementById("prj-5");
+var prj5Git = document.getElementById("prj-1-git");
 
-
-prj1.addEventListener('click', function () {
-    location.href = 'https://github.com/UnDuhDuhSea/My-Portfolio';
-})
+prj1.addEventListener("click", function () {
+  location.href = "https://github.com/UnDuhDuhSea/CacheApp";
+});
 // prj1Git.addEventListener('click', function () {
 //     location.href = 'https://d606n6k.github.io/recipe-shuffle/';
 // })
 
-prj2.addEventListener('click', function () {
-    location.href = ' https://github.com/UnDuhDuhSea/Weather-Dashboard';
-})
+prj2.addEventListener("click", function () {
+  location.href = " https://github.com/d606n6k/recipe-shuffle";
+});
 // prj2Git.addEventListener('click', function () {
 //     location.href = 'https://unduhduhsea.github.io/Weather-Dashboard/';
 // })
 
-prj3.addEventListener('click', function () {
-    location.href = 'https://github.com/UnDuhDuhSea/Work-Day-Scheduler';
-})
+prj3.addEventListener("click", function () {
+  location.href = " https://github.com/UnDuhDuhSea/Weather-Dashboard";
+});
+// prj2Git.addEventListener('click', function () {
+//     location.href = 'https://unduhduhsea.github.io/Weather-Dashboard/';
+// })
+
+prj4.addEventListener("click", function () {
+  location.href = "https://github.com/UnDuhDuhSea/Work-Day-Scheduler";
+});
+// prj3Git.addEventListener('click', function () {
+//     location.href = 'https://unduhduhsea.github.io/Work-Day-Scheduler/';
+// })
+
+prj5.addEventListener("click", function () {
+  location.href = "https://github.com/UnDuhDuhSea/Tech-Blog";
+});
 // prj3Git.addEventListener('click', function () {
 //     location.href = 'https://unduhduhsea.github.io/Work-Day-Scheduler/';
 // })
